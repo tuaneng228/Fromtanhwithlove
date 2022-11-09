ff67d3bf686195fc8c0c744af4f6cbe39d902179
tuaneng228 <114272936+tuaneng228@users.noreply.github.com>
2022-11-09 18:53:21 +0700

    ok

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hellu Thanh Thao!',
+    introDesc: `Welcome to my heart <3`,
+    btnIntro: 'From TuanEng with love',
+    title: 'Cậu có muốn tiến sâu vào trái tim mình khonggg 😙',
+    desc: 'Có thì làm theo hướng dẫn nháaa ',
+    btnYes: 'Đi theo <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Em thấy anh thế nào?',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Quá tốt bụng, quá sức tinh tế hihi <33333333',
+    mess: 'WOAAAA 🥰. Anh biết mà 😘.',
+    messDesc: 'Chúc em buổi tối vui vẻ nha <3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/nta228' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
